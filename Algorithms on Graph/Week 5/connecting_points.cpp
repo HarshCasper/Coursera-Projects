1f01fcc159613047ffb026a91204c0a33b910bf8
Harsh Bardhan Mishra <47351025+HarshCasper@users.noreply.github.com>
2020-04-02 19:01:59 +0530

    Create connecting_points.cpp

---
+++ Algorithms on Graph/Week 5/connecting_points.cpp
@@ -0,0 +1,63 @@
+#include <algorithm>
+#include <cmath>
+#include <iomanip>
+#include <iostream>
+#include <vector>
+
+using std::vector;
+struct Node {
+  int start, end;
+  double dis;
+  Node(int x, int y, double d) : start(x), end(y), dis(d){};
+  bool operator<(const Node &a) const { return dis < a.dis; }
+
+};
+int Find(int x, vector<int> &parent) {
+  if (x != parent[x])
+    parent[x] = Find(parent[x], parent);
+  return parent[x];
+}
+void Union(int x, int y, vector<int> &parent) {
+  int left = Find(x, parent);
+  int right = Find(y, parent);
+  if (left != right) {
+    parent[right] = left;
+  }
+}
+double minimum_distance(vector<int> x, vector<int> y) {
+  double result = 0.;
+  // write your code here
+  vector<int> parent(x.size());
+  vector<Node> q;
+  for (size_t i = 0; i < x.size(); i++)
+    parent[i] = i;
+  for (size_t i = 0; i < x.size(); i++) {
+    for (size_t j = i + 1; j < x.size(); j++) {
+      double dis =
+          sqrt((x[i] - x[j]) * (x[i] - x[j]) + (y[i] - y[j]) * (y[i] - y[j]));
+      q.push_back(Node(i, j, dis));
+    }
+  }
+  std::sort(q.begin(), q.end());
+
+  for (size_t i = 0; i < q.size(); i++) {
+    Node v = q[i];
+    if (Find(v.start, parent) !=
+        Find(v.end, parent)) {
+      result += v.dis;
+      Union(v.start, v.end, parent); 
+    }
+  }
+  return result;
+}
+
+int main() {
+  size_t n;
+  std::cin >> n;
+  vector<int> x(n), y(n);
+  for (size_t i = 0; i < n; i++) {
+    std::cin >> x[i] >> y[i];
+  }
+  std::cout << std::setprecision(10) << minimum_distance(x, y) << std::endl;
+  return 0;
+}
