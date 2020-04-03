ed320c3264286a285745dd6c3c3f14507703284c
Harsh Bardhan Mishra <47351025+HarshCasper@users.noreply.github.com>
2020-04-03 21:10:35 +0530

    Create reconstruct_tree.cpp

---
+++ Algorithms on Strings/Programming Assignment 2/reconstruct_tree.cpp
@@ -0,0 +1,21 @@
+#python3
+import sys
+
+
+def inverse_bwt(bwt):
+    last = [(val, idx) for (idx, val) in enumerate(bwt)]
+    first = sorted(last)
+    first_to_last = {f: l for f, l in zip(first, last)}
+
+    next = first[0]
+    result = ''
+    for i in range(len(bwt)):
+        result += next[0]
+        next = first_to_last[next]
+
+    return result[::-1]
+
+
+if __name__ == '__main__':
+    bwt = sys.stdin.readline().strip()
+    print(inverse_bwt(bwt))
